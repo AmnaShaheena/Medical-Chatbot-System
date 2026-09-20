1e54760b6623f0a5870842ecbfd711e560e2a4bb
AmnaShaheena <amnashaheena02@gmail.com>
2026-09-20 17:12:11 +0530

    installed requirements

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='medical_chatbot_system',
+    version='1.0.0',
+    author='Amna Shaheena',
+    author_email='amnashaheena02@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+
+)
